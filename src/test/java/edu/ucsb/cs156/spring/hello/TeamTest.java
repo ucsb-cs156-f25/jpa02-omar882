fa5853c0498c6a77ef47a6b44facc748a29aed4d
Omar Aboutaleb <omaraboutaleb06@gmail.com>
2025-10-14 23:09:13 -0700

    added more tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,11 +17,39 @@ public class TeamTest {
 
     @Test
     public void getName_returns_correct_name() {
-       assert(team.getName().equals("test-team"));
+       assertEquals("test-team", team.getName());
+    }
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+   
+    @Test
+    public void equals_returns_correct_result() {
+        assertTrue(team.equals(team));
+        Team team2 = new Team("test-team");
+        assertTrue(team.equals(team2));
+        Team team3 = new Team("different-team");
+        assertTrue(!team.equals(team3));
+        assertTrue(!team.equals(null));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void hashCode_returns_correct_result() {
+        Team team2 = new Team("test-team");
+        assertEquals(team.hashCode(), team2.hashCode());
+        Team team3 = new Team("test-team");
+        team2.addMember("Bob");
+        team3.addMember("Bob");
+        assertEquals(team2.hashCode(), team3.hashCode());
+    
+        int result = team.hashCode();
+        int expectedResult = -1226298695;
+        assertEquals(expectedResult, result);
+
+
+
+    }
+
 
 }

d93830187e76559815fa1206f65c960388da123f
Omar Aboutaleb <omaraboutaleb06@gmail.com>
2025-10-14 22:31:52 -0700

    updated with actual names

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-f25-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Omar A.";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "omar882";
     }
 
     /**
@@ -40,13 +40,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f25-16");
+        team.addMember("OMAR AHMED");
+        team.addMember("MIHIR");
+        team.addMember("ALEX ");
+        team.addMember("QUINN THEODORE ROGER");
+        team.addMember("SAQIF AYAAN");
+        team.addMember("WILLIAM PAUL");
         return team;
     }
 }

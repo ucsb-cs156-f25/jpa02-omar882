c96c980d4dcb05c6620ac045782d4de45d898240
Omar Aboutaleb <omaraboutaleb06@gmail.com>
2025-10-14 22:35:00 -0700

    updated with github id and test github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,10 @@ public class DeveloperTest {
         assertEquals("Omar A.", Developer.getName());
     }
 
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("omar882", Developer.getGithubId());
+    }
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
